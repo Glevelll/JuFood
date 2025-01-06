03bc480c0713ce0e04a9ee6629c735d253aae207
gleb <gleb.nikishin.2003@gmail.com>
2025-01-06 23:09:08 +0300

    feat: Поднял версии библиотек и поменял kapt на ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.project.jufood"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.project.jufood"
@@ -53,30 +53,30 @@ android {
 dependencies {
 
     implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.0")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.2")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("com.google.accompanist:accompanist-insets:0.17.0")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("org.kodein.di:kodein-di:7.1.0")
     implementation("org.kodein.di:kodein-di-framework-android-x:7.1.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
